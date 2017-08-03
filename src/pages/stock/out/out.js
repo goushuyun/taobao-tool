72db7bc02d42d8e19f6f62bd78850c2a9f419c42
Wang-Kai <persistence2013@sina.com>
2017-08-03 16:03:04 +0800

    modify

--- src/pages/stock/out/out.js
+++ src/pages/stock/out/out.js
@@ -1,44 +1,48 @@
 export default {
-  data() {
-    return {
-      isbn: '',
-      out: '',
-      tableData: [{
-        isbn: '9787123456789',
-        title: '数据结构',
-        price: '15.00',
-        stock: '大仓库',
-        shelf: 'A货架',
-        floor: '1层',
-        moment: 10,
-        out: 1
-      }, {
-        isbn: '9787123456789',
-        title: '数据结构',
-        price: '15.00',
-        stock: '大仓库',
-        shelf: 'A货架',
-        floor: '1层',
-        moment: 10,
-        out: 1
-      }, {
-        isbn: '9787123456789',
-        title: '数据结构',
-        price: '15.00',
-        stock: '大仓库',
-        shelf: 'A货架',
-        floor: '1层',
-        moment: 10,
-        out: 1
-      }]
-    }
-  },
-  methods: {
-    getData() {
+	methods : {
+		out() {
+            console.log('kai');
+        }
+	},
 
-    },
-    test() {
-
-    }
-  }
+	data() {
+		return {
+			isbn: '',
+			out: '',
+			tableData: [
+				{
+					isbn: '9787123456789',
+					title: '数据结构',
+					price: '15.00',
+					stock: '大仓库',
+					shelf: 'A货架',
+					floor: '1层',
+					moment: 10,
+					out: 1
+				}, {
+					isbn: '9787123456789',
+					title: '数据结构',
+					price: '15.00',
+					stock: '大仓库',
+					shelf: 'A货架',
+					floor: '1层',
+					moment: 10,
+					out: 1
+				}, {
+					isbn: '9787123456789',
+					title: '数据结构',
+					price: '15.00',
+					stock: '大仓库',
+					shelf: 'A货架',
+					floor: '1层',
+					moment: 10,
+					out: 1
+				}
+			]
+		}
+	},
+	methods : {
+		getData() {},
+		test() {}
+	}
 }
