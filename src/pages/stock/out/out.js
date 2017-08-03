c751f776a1e2ef8645c57460da9cd53fd1b730b4
Wang-Kai <persistence2013@sina.com>
2017-08-03 21:23:52 +0800

    complete style at out page

--- src/pages/stock/out/out.js
+++ src/pages/stock/out/out.js
@@ -1,48 +1,11 @@
 export default {
 	methods : {
-		out() {
+		output() {
             console.log('kai');
-        }
-	},
+        },
 
-	data() {
-		return {
-			isbn: '',
-			out: '',
-			tableData: [
-				{
-					isbn: '9787123456789',
-					title: '数据结构',
-					price: '15.00',
-					stock: '大仓库',
-					shelf: 'A货架',
-					floor: '1层',
-					moment: 10,
-					out: 1
-				}, {
-					isbn: '9787123456789',
-					title: '数据结构',
-					price: '15.00',
-					stock: '大仓库',
-					shelf: 'A货架',
-					floor: '1层',
-					moment: 10,
-					out: 1
-				}, {
-					isbn: '9787123456789',
-					title: '数据结构',
-					price: '15.00',
-					stock: '大仓库',
-					shelf: 'A货架',
-					floor: '1层',
-					moment: 10,
-					out: 1
-				}
-			]
+		isbn_search(){
+			console.log(this.isbn);
 		}
-	},
-	methods : {
-		getData() {},
-		test() {}
 	}
 }
--- src/pages/stock/out/out.vue
+++ src/pages/stock/out/out.vue
@@ -1,7 +1,5 @@
-<style lang="scss">
-
+<style lang="scss" scoped>
 @import "./out.scss";
-
 </style>
 
 <template lang="html">
@@ -11,28 +9,67 @@
     <div class="content_inner">
         <el-form :inline="true">
             <el-form-item label="ISBN">
-                <el-input id="isbn" size="small" v-model.trim="isbn" @keyup.enter.native="getData"></el-input>
+                <el-input id="isbn" size="small" icon="search" autofocus :on-icon-click="isbn_search" v-model.trim="isbn" @keyup.enter.native="isbn_search"></el-input>
             </el-form-item>
             <el-form-item label="库存总量：" label-width="200px">
                 <span>11</span>
             </el-form-item>
             <el-form-item label="出库量：" label-width="100px">
-                <el-input style="width: 80px;" size="small" v-model.trim="out" @keyup.enter.native="test"></el-input>
+                <el-input style="width: 80px;" size="small"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button size="small" type="primary" @click.native="out">出库</el-button>
+                <el-button size="small" type="primary" @click="output">出库</el-button>
             </el-form-item>
         </el-form>
-        <el-table :data="tableData" border style="width: 100%">
-            <el-table-column prop="isbn" label="ISBN" width="160"></el-table-column>
-            <el-table-column prop="title" label="书名"></el-table-column>
-            <el-table-column prop="price" label="售价" width="80"></el-table-column>
-            <el-table-column prop="stock" label="库位"></el-table-column>
-            <el-table-column prop="shelf" label="架位"></el-table-column>
-            <el-table-column prop="floor" label="层数" width="80"></el-table-column>
-            <el-table-column prop="moment" label="库存量" width="80"></el-table-column>
-            <el-table-column prop="out" label="出库量" width="80"></el-table-column>
-        </el-table>
+
+        <el-row :gutter="10">
+            <el-col :span="17">
+                <el-table  border style="width: 100%">
+                        <el-table-column
+                        type="selection"
+                        width="55">
+                    </el-table-column>
+                    <el-table-column label="库位-货架-层数"></el-table-column>
+                    <el-table-column label="库存量" width="80"></el-table-column>
+                    <el-table-column label="出货量"></el-table-column>
+                </el-table>
+            </el-col>
+
+            <el-col :span="7">
+                <div class="book_info">
+
+                    <div class="image_box">
+                        <img class="book_image" src="https://img11.360buyimg.com/n1/s200x200_jfs/t5185/254/1456634708/110457/1e877299/59106ff8N763037e0.jpg" alt="">
+                    </div>
+
+                    <ul class="info_detail">
+                        <li>
+                            <span class="key">书名</span>
+                            <span class="val">SQL必知必会 第4版</span>
+                        </li>
+                        <li>
+                            <span class="key">作者</span>
+                            <span class="val">[美] Ben Forta 著；钟鸣，刘晓霞 译</span>
+                        </li>
+                        <li>
+                            <span class="key">出版社</span>
+                            <span class="val">人民邮电出版社</span>
+                        </li>
+                        <li>
+                            <span class="key">定价</span>
+                            <span class="val">29.00</span>
+                        </li>
+                        <li>
+                            <span class="key">isbn</span>
+                            <span class="val">9787115313980</span>
+                        </li>
+                    </ul>
+
+                </div>
+            </el-col>
+
+        </el-row>
+
     </div>
 </div>
 
@@ -45,6 +82,7 @@ export default {
     mixins: [mixin],
     data(){
         return {
+            isbn: '',
 
         }
     }
