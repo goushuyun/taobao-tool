d6aa1845767fc1952b88711cb288bb14f10f6380
fengzhongsen <fengzhongsen@gmail.com>
2017-08-09 21:45:51 +0800

    setting

--- src/pages/stock/setting/setting.js
+++ src/pages/stock/setting/setting.js
@@ -1,6 +1,7 @@
 import {
     priceFloat,
-    priceInt
+    priceInt,
+    unique
 } from '../../../assets/script/utils.js'
 import axios from "../../../config/http.js"
 export default {
@@ -9,35 +10,244 @@ export default {
             warehouse: '',
             shelf: '',
             floor: '',
-            select: '1',
+            shelf_disabled: true,
+            floor_disabled: true,
+            location_id: '',
             locations: [],
+            // 检测位置是否变化，变了就去获取get_location_id，不变就不获取
+            to_get_location_id: false,
 
             loading: false,
+            page: 1,
             size: 10,
-            total_count: 100
+            total: 0, //总库存量
+            total_count: 0,
+
+            input: '',
+            select: 'isbn',
+            goods: [],
+            goods_page: 1,
+            goods_size: 10,
+            goods_total_count: 0,
+
+            show_detail: false
         }
     },
     mounted() {
-        this.locationFazzyQuery()
+        this.getLocationStock()
+    },
+    watch: {
+        warehouse(value) {
+            if (value) {
+                this.shelf_disabled = false
+                this.to_get_location_id = true
+            } else {
+                this.shelf_disabled = true
+                this.to_get_location_id = false
+            }
+            this.shelf = ''
+            this.getLocationStock()
+        },
+        shelf(value) {
+            if (value) {
+                this.floor_disabled = false
+                this.to_get_location_id = true
+            } else {
+                this.floor_disabled = true
+                this.to_get_location_id = false
+            }
+            this.floor = ''
+            this.getLocationStock()
+        },
+        floor(value) {
+            if (value) {
+                this.to_get_location_id = true
+            }
+            this.to_get_location_id = true
+            this.getLocationStock()
+        }
     },
     methods: {
-        locationFazzyQuery() {
-            axios.post('/v1/stock/location_fazzy_query', {
-                "warehouse": "A", //仓库名称 optional
-                "shelf": "B", //货架名称 optional
-                "floor": "1" //层数 optional
+        getLocationStock() {
+            axios.post('/v1/stock/get_location_stock', {
+                "warehouse": this.warehouse, //仓库名称 optional
+                "shelf": this.shelf, //货架名称 optional
+                "floor": this.floor, //层数 optional
+                "page": this.page,
+                "size": this.size
             }).then(resp => {
                 if (resp.data.message == 'ok') {
                     var data = resp.data.data
+                    this.total = resp.data.total
+                    this.total_count = resp.data.total_count
                     this.locations = data
+                }
+            })
+        },
+        queryWarehouse(query, cb) {
+            axios.post('/v1/stock/location_fazzy_query', {
+                "warehouse": ""
+            }).then(resp => {
+                if (resp.data.message == 'ok') {
+                    let data = resp.data.data
+                    var list = []
+                    data.forEach(el => {
+                        list.push(el.warehouse)
+                    })
+                    list = unique(list)
+                    var result = []
+                    list.forEach(warehouse => {
+                        result.push({
+                            value: warehouse
+                        })
+                    })
+                    cb(result)
+                }
+            })
+        },
+        queryShelf(query, cb) {
+            axios.post('/v1/stock/location_fazzy_query', {
+                "warehouse": this.warehouse
+            }).then(resp => {
+                if (resp.data.message == 'ok') {
+                    let data = resp.data.data
+                    var list = []
+                    data.forEach(el => {
+                        list.push(el.shelf)
+                    })
+                    list = unique(list)
+                    var result = []
+                    list.forEach(shelf => {
+                        result.push({
+                            value: shelf
+                        })
+                    })
+                    cb(result)
+                }
+            })
+        },
+        queryFloor(query, cb) {
+            axios.post('/v1/stock/location_fazzy_query', {
+                "warehouse": this.warehouse,
+                "shelf": this.shelf
+            }).then(resp => {
+                if (resp.data.message == 'ok') {
+                    let data = resp.data.data
+                    var list = []
+                    data.forEach(el => {
+                        list.push(el.floor)
+                    })
+                    list = unique(list)
+                    var result = []
+                    list.forEach(floor => {
+                        result.push({
+                            value: floor
+                        })
+                    })
+                    cb(result)
                 }
             })
         },
         handleSizeChange(size) {
             this.size = size
+            this.getLocationStock()
         },
         handleCurrentChange(page) {
             this.page = page
+            this.getLocationStock()
+        },
+        // 查看此货架书籍
+        showDetail(index) {
+            this.location_id = this.locations[index].location_id
+            this.reset()
+            this.show_detail = true
+        },
+        searchGoods(index) {
+            this.loading = true
+            var request = {
+                "page": this.goods_page,
+                "size": this.goods_size,
+                "order_by": "stock", // stock
+                "sequence": "asc", // desc: 数量从大到小  asc：数量从小到大
+                "location_id": this.location_id
+            }
+            if (this.select === 'greater' || this.select === 'less') {
+                request['compare'] = this.select
+                request['stock'] = this.input
+            } else if (this.select === 'isbn') {
+                var reg = /^978\d{10}_\d{2}$/
+                if (reg.test(this.input)) {
+                    var isbn_no = this.input.split('_')
+                    request.isbn = isbn_no[0]
+                    request.book_no = isbn_no[1]
+                    request.book_cate = 'poker'
+                } else {
+                    request.isbn = this.input
+                }
+            } else {
+                request[this.select] = this.input
+            }
+            axios.post('/v1/stock/search_goods', request).then(resp => {
+                if (resp.data.message == 'ok') {
+                    var data = resp.data.data.map(el => {
+                        el.isbn_no = (el.book_no != '' && el.book_no != '00') ? (el.isbn + '_' + el.book_no) : el.isbn
+                        el.this_stock = 0
+                        el.price = priceFloat(el.price)
+                        return el
+                    })
+                    this.goods_total_count = resp.data.total_count
+                    if (data.length == 0) {
+                        this.goods = data
+                        this.loading = false
+                        return
+                    }
+                    var self = this
+                    var count = 0
+                    for (var i = 0; i < data.length; i++) {
+                        (function(index) {
+                            axios.post('/v1/stock/list_goods_all_locations', {
+                                "goods_id": data[index].goods_id, //required
+                                "location_id": self.location_id,
+                                "page": 1,
+                                "size": 10
+                            }).then(resp => {
+                                if (resp.data.message == 'ok') {
+                                    data[index].this_stock = resp.data.data[0].stock
+                                    count++
+                                    if (count == data.length) {
+                                        self.goods = data
+                                        self.loading = false
+                                    }
+                                }
+                            })
+                        })(i)
+                    }
+                }
+            })
+        },
+        openBookDialog(index) {
+            var book = this.goods[index]
+            this.$router.push({
+                name: 'detail',
+                params: {
+                    book
+                }
+            })
+        },
+        reset() {
+            this.input = ''
+            this.select = 'isbn'
+            this.goods_page = 1
+            this.goods_size = 10
+            this.searchGoods()
+        },
+        handleGoodsSizeChange(size) {
+            this.goods_size = size
+            this.searchGoods()
+        },
+        handleGoodsCurrentChange(page) {
+            this.goods_page = page
+            this.searchGoods()
         }
     }
 }
--- src/pages/stock/setting/setting.vue
+++ src/pages/stock/setting/setting.vue
@@ -2,18 +2,16 @@
   <div class="container">
     <div class="content_top_bar">仓库设置</div>
     <div class="content_inner">
-      <div class="gsy-card">
-        <el-row class="gsy-header">
-          <el-col>
-            <span>仓库位置：</span>
-            <el-input size="small" placeholder="库位" style="width: 100px;" v-model="warehouse"></el-input> -
-            <el-input size="small" placeholder="架位" style="width: 100px;" v-model="shelf"></el-input> -
-            <el-input size="small" placeholder="层数" style="width: 100px;" v-model="floor"></el-input>
-            <el-button size="small" type="primary" style="margin-left: 12px;">搜索</el-button>
-            <el-button size="small" type="primary">新增货架</el-button>
-            <el-button size="small" type="primary">生成货架条码</el-button>
-          </el-col>
-        </el-row>
+      <div v-show="!show_detail" class="gsy-card">
+        <div class="gsy-header">
+          <div class="location">
+            <label style="margin-right: 7px;">库存位置</label>
+            <el-autocomplete size="small" class="inline-input" style="width: 100px;" v-model="warehouse" :fetch-suggestions="queryWarehouse" placeholder="库位"></el-autocomplete> -
+            <el-autocomplete size="small" class="inline-input" style="width: 100px;" v-model="shelf" :disabled="shelf_disabled" :fetch-suggestions="queryShelf" placeholder="架位"></el-autocomplete> -
+            <el-autocomplete size="small" class="inline-input" style="width: 100px;" v-model="floor" :disabled="floor_disabled" :fetch-suggestions="queryFloor" placeholder="层数"></el-autocomplete>
+            <span class="total">总库存量：{{total}} 本</span>
+          </div>
+        </div>
         <div class="gsy-body" v-loading="loading" element-loading-text="拼命加载中">
           <el-table :data="locations" border style="width: 100%">
             <el-table-column label="库位 - 架位 - 层数">
@@ -21,10 +19,10 @@
                 {{scope.row.warehouse}} - {{scope.row.shelf}} - {{scope.row.floor}}
               </template>
             </el-table-column>
-            <el-table-column prop="total_stock" label="库存量" width="80"></el-table-column>
+            <el-table-column prop="total_stock" label="库存量" width="150"></el-table-column>
             <el-table-column label="操作" width="260">
               <template scope="scope">
-                <el-button type="primary" size="small">查看此货架书籍</el-button>
+                <el-button type="primary" size="small" @click="showDetail(scope.$index)">查看此货架书籍</el-button>
                 <el-button type="primary" size="small">修改</el-button>
                 <el-button type="primary" size="small">删除</el-button>
               </template>
@@ -33,6 +31,50 @@
         </div>
         <div class="gsy-footer">
           <el-pagination :page-sizes="[10, 20, 50, 100]" :page-size="size" layout="total, sizes, prev, pager, next, jumper" :total="total_count" @size-change="handleSizeChange" @current-change="handleCurrentChange">
+          </el-pagination>
+        </div>
+      </div>
+      <div v-show="show_detail" class="gsy-card">
+        <el-row class="gsy-header">
+          <el-col :span="24" class="goods">
+            <el-input size="small" placeholder="请输入内容" v-model="input" @keyup.enter.native="searchGoods">
+              <el-select v-model="select" slot="prepend" placeholder="请选择">
+                <el-option label="ISBN" value="isbn"></el-option>
+                <el-option label="书名" value="title"></el-option>
+                <el-option label="作者" value="author"></el-option>
+                <el-option label="出版社" value="publisher"></el-option>
+                <el-option label="库存量大于等于" value="greater"></el-option>
+                <el-option label="库存量小于" value="less"></el-option>
+              </el-select>
+            </el-input>
+            <el-button size="small" type="primary" @click="searchGoods()">筛选</el-button>
+            <el-button size="small" type="primary" @click="reset">重置</el-button>
+            <el-button style="float: right;" type="text" size="small" icon="close" @click="show_detail = false"></el-button>
+          </el-col>
+        </el-row>
+        <div class="gsy-body" v-loading="loading" element-loading-text="拼命加载中">
+          <el-table :data="goods" border style="width: 100%">
+            <el-table-column type="index" width="60"></el-table-column>
+            <el-table-column label="封面" width="100">
+              <template scope="scope" >
+                <div class="image_wrap">
+                  <img :src="scope.row.image == '' ? 'http://image.goushuyun.cn/book.png' : ('http://taoimage.goushuyun.cn/' + scope.row.image)" class="image"></img>
+                </div>
+              </template>
+            </el-table-column>
+            <el-table-column prop="isbn_no" label="ISBN" width="170"></el-table-column>
+            <el-table-column prop="title" label="书名"></el-table-column>
+            <el-table-column prop="this_stock" label="此库存量" sortable width="120"></el-table-column>
+            <el-table-column prop="stock" label="总库存量" width="120"></el-table-column>
+            <el-table-column label="操作" width="100">
+              <template scope="scope">
+                <el-button type="text" @click="openBookDialog(scope.$index)">查看详情</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+        <div class="gsy-footer">
+          <el-pagination :page-sizes="[10, 20, 50, 100]" :page-size="goods_size" layout="total, sizes, prev, pager, next, jumper" :total="goods_total_count" @size-change="handleGoodsSizeChange" @current-change="handleGoodsCurrentChange">
           </el-pagination>
         </div>
       </div>
